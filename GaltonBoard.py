1143f5980e9d5a5861b0d514c5fa19c3168a635a
TimoKropp <47632407+TimoKropp@users.noreply.github.com>
2020-08-11 18:43:22 +0200

    Update GaltonBoard.py

--- GaltonBoard.py
+++ GaltonBoard.py
@@ -1,36 +1,22 @@
 import pygame
 import random
-<<<<<<< HEAD
+import os
 
 # simulation settings
-rad        =  1            # radius of a Ball
-N_Balls    = 3000          # number of Balls
-size       = 300           # window size, width = size/2, hight = size
-width      = 2             # width of a bar
-clock_tick = 100           # clock tick, simulation speed
-level_red  = 300           # level to trap and count Balls in red (y-position in window)
-level_blue = 500           # level to trap and count Balls in blue (y-position in window)
-final_level = level_blue   # end of Galton Board
+rad         =  1            # radius of a Ball
+N_Balls     = 3000          # number of Balls
+size        = 300           # window size, width = size/2, hight = size
+width       = 2             # width of a bar
+clock_tick  = 100           # clock tick, simulation speed
+level_red   = 300           # level to trap and count Balls in red (y-position in window)
+level_blue  = 500           # level to trap and count Balls in blue (y-position in window)
+final_level = level_blue    # end of Galton Board
 
 # Define some colors for drawing
 black    = (   0,   0,   0)
 white    = ( 255, 255, 255)
 red      = ( 255,   0,   0)
 blue     = (   0,   0, 255)
-=======
-import os
-
-# Define some colors
-black    = (   0,   0,   0)
-white    = ( 255, 255, 255)
-
-rad = 1             #radius for a ball
-N_Balls = 5000      #number of balls
-size = 300          #window size, width = size/2, hight = size
-level = 500         #ground level to draw bars
-width = 1           #width of a bar
-clock_tick = 100    #clock tick, simulation speed
->>>>>>> 409b6292830023659cb53564f86d8297cbb4d532
 
 class Ball():
     def __init__(self, x, y, rad):    
@@ -63,51 +49,35 @@ class Bar():
         pygame.draw.line(self.screen, self.color, [self.pos, self.level],[self.pos, self.level-self.height], self.width)
 
 
-<<<<<<< HEAD
-### initializing ###
+
+# Initializing 
 Balls = []
 BarsRed = []
 BarsBlue = []
 
-=======
-# Initializing 
->>>>>>> 409b6292830023659cb53564f86d8297cbb4d532
 pygame.display.init()
-w_h = [size,2 * size] # aspect ration: height = 2* width
+w_h = [size,2 * size] # aspect ration: height = 2 * width
 screen = pygame.display.set_mode(w_h)
 pygame.display.set_caption("Galton Animation")
 stop = False
 clock = pygame.time.Clock()
 
-<<<<<<< HEAD
-
-### create Balls ####
+# create Balls
 for k in range(0, N_Balls):
     x = int( size / 2)
     y = 1 - (N_Balls - k)
     Balls.append(Ball( x, y, rad))
-=======
-# Create Balls 
-for k in range(0,N_Balls):
-        x=int(size/2)
-        y=1-(N_Balls-k)
-        Balls.append(Ball(x,y,rad))
->>>>>>> 409b6292830023659cb53564f86d8297cbb4d532
 
-# Create Bars       
+# create Bars       
 for j in range(0,size):
-    BarsRed.append(Bar(level_red, width, j, 0,  red)) # init Bars with 0 height: creates horizontal line at level_red
-    BarsBlue.append(Bar(level_blue,width, j, 0, blue)) # init Bars with 0 height: creates horizontal line at level_blue
+    BarsRed.append(Bar(level_red, width, j, 0,  red))   # init Bars with 0 height: creates horizontal line at level_red
+    BarsBlue.append(Bar(level_blue, width, j, 0, blue)) # init Bars with 0 height: creates horizontal line at level_blue
 
-<<<<<<< HEAD
-#### loop ####
-=======
 # Loop 
->>>>>>> 409b6292830023659cb53564f86d8297cbb4d532
-while stop == False: # check for exit
-    for event in pygame.event.get(): # User did something
-        if event.type == pygame.QUIT: # If user clicked close
-            stop = True # Flag that we are done so we exit this loop
+while stop == False:                    # check for exit
+    for event in pygame.event.get():    # User did something
+        if event.type == pygame.QUIT:   # If user clicked close
+            stop = True                 # Flag that we are done so we exit this loop
             
     screen.fill(black)
     
